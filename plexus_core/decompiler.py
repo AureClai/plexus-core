015d455c84ef55d07e2e2b5d8e168cbb19df4504
aureclai.play <aureclai.play@gmail.com>
2025-06-21 14:32:12 +0200

    Everything is ok here !

--- plexus_core/decompiler.py
+++ plexus_core/decompiler.py
@@ -11,13 +11,11 @@ REVERSE_CMPOP_MAP = {
     ast.LtE: "<=", ast.Gt: ">", ast.GtE: ">=",
 }
 
-
 class JSONDecompiler(ast.NodeVisitor):
     """Walks a Python AST and converts it into a Plexus JSON graph representation."""
-
     def __init__(self):
-        self.nodes = []
-        self.connections = []
+        self.statement_nodes = []
+        self.expression_nodes = []
         self.variable_providers = {}
         self.expression_map = {}
         self._node_count = 0
@@ -27,7 +25,6 @@ class JSONDecompiler(ast.NodeVisitor):
         return f"{prefix}-{self._node_count}"
 
     def _get_input_data_from_ast(self, value_node: ast.AST) -> dict:
-        """Analyzes an AST value node and returns the core of an input dict."""
         if isinstance(value_node, ast.Constant):
             return {"value": repr(value_node.value)}
         elif isinstance(value_node, ast.Name):
@@ -35,9 +32,7 @@ class JSONDecompiler(ast.NodeVisitor):
             if var_name in self.variable_providers:
                 return {"link": self.variable_providers[var_name]}
             else:
-                raise NameError(
-                    f"Variable '{var_name}' used before assignment.")
-        # FIX: Explicitly handle collection literals by unparsing them.
+                raise NameError(f"Variable '{var_name}' used before assignment.")
         elif isinstance(value_node, (ast.List, ast.Tuple, ast.Set, ast.Dict)):
             return {"value": ast.unparse(value_node)}
         else:
@@ -46,66 +41,69 @@ class JSONDecompiler(ast.NodeVisitor):
             return {"link": self.expression_map.get(value_node)}
 
     def _decompile_body(self, body_nodes: list) -> list:
-        original_nodes_list = self.nodes
-        self.nodes = []
+        original_stmt_nodes = self.statement_nodes
+        self.statement_nodes = []
         for stmt in body_nodes:
             self.visit(stmt)
-        newly_added_nodes = self.nodes
-        self.nodes = original_nodes_list
+        newly_added_nodes = self.statement_nodes
+        self.statement_nodes = original_stmt_nodes
         return newly_added_nodes
 
     def visit(self, node):
         method = 'visit_' + node.__class__.__name__
         visitor = getattr(self, method, self.generic_visit)
         return visitor(node)
-
+    
     def generic_visit(self, node):
         super().generic_visit(node)
 
     def visit_Assign(self, node: ast.Assign):
-        if len(node.targets) != 1:
-            raise NotImplementedError(
-                "Unsupported syntax: Assignment to multiple targets is not supported.")
+        self.visit(node.value)
         target = node.targets[0]
-        if isinstance(target, (ast.Tuple, ast.List)):
-            raise NotImplementedError(
-                "Unsupported syntax: Assignment to multiple targets is not supported.")
         if not isinstance(target, ast.Name):
-            raise NotImplementedError(
-                "Unsupported syntax: Assignment to non-variable targets is not supported.")
+            raise NotImplementedError("Unsupported syntax: Assignment to non-simple targets")
+        
         var_name = target.id
         node_id = self._get_unique_id("assign")
         input_data = self._get_input_data_from_ast(node.value)
-        input_data['name'] = 'value'
-        plexus_node = {"id": node_id, "type": "variable_assign",
-                       "value": var_name, "inputs": [input_data]}
-        self.nodes.append(plexus_node)
+        input_data['name'] = 'value' # Ensure input has a name for the compiler
+        plexus_node = {
+            "id": node_id,
+            "type": "variable_assign",
+            "value": var_name,
+            "inputs": [input_data],
+            "is_intended_statement": True # Assign is always a statement
+        }
+        self.statement_nodes.append(plexus_node)
         self.variable_providers[var_name] = node_id
 
     def visit_Expr(self, node: ast.Expr):
-        if isinstance(node.value, ast.Call) and isinstance(node.value.func, ast.Name) and node.value.func.id == 'print':
-            self.visit_Print(node.value)
+        if isinstance(node.value, ast.Call):
+            self.visit_Call(node.value, is_statement=True)
         else:
             self.generic_visit(node)
-
+    
     def visit_If(self, node: ast.If):
         node_id = self._get_unique_id("if")
         self.visit(node.test)
         test_input = self._get_input_data_from_ast(node.test)
         test_input['name'] = 'test'
         body_plexus_nodes = self._decompile_body(node.body)
-        orelse_plexus_nodes = self._decompile_body(node.orelse)
-        plexus_node = {"id": node_id, "type": "if_statement", "inputs": [
-            test_input], "body": body_plexus_nodes, "orelse": orelse_plexus_nodes}
-        self.nodes.append(plexus_node)
+        orelse_plexus_nodes = self._decompile_body(node.orelse) if node.orelse else []
+        plexus_node = {
+            "id": node_id,
+            "type": "if_statement",
+            "inputs": [test_input],
+            "body": body_plexus_nodes,
+            "orelse": orelse_plexus_nodes,
+            "is_intended_statement": True # If is always a statement
+        }
+        self.statement_nodes.append(plexus_node)
 
     def visit_For(self, node: ast.For):
-        if node.orelse:
-            raise NotImplementedError("For/else loops are not supported.")
-        if not isinstance(node.target, ast.Name):
-            raise NotImplementedError(
-                "Looping with complex targets (e.g., for x, y in ...)")
-
+        if node.orelse: raise NotImplementedError("For/else loops are not supported.")
+        if not isinstance(node.target, ast.Name): raise NotImplementedError("Looping with complex targets is not supported.")
+        
         node_id = self._get_unique_id("for_loop")
         target_var_name = node.target.id
         self.visit(node.iter)
@@ -119,59 +117,83 @@ class JSONDecompiler(ast.NodeVisitor):
         else:
             del self.variable_providers[target_var_name]
         plexus_node = {
-            "id": node_id, "type": "for_loop", "target_variable": target_var_name,
-            "inputs": [iter_input], "body": body_plexus_nodes
+            "id": node_id,
+            "type": "for_loop",
+            "target_variable": target_var_name,
+            "inputs": [iter_input],
+            "body": body_plexus_nodes,
+            "is_intended_statement": True # For is always a statement
         }
-        self.nodes.append(plexus_node)
+        self.statement_nodes.append(plexus_node)
 
-    def visit_Print(self, node: ast.Call):
-        if len(node.args) != 1:
-            raise NotImplementedError(
-                f"Unsupported syntax: print() calls with {len(node.args)} arguments are not supported.")
-        node_id = self._get_unique_id("print")
-        input_data = self._get_input_data_from_ast(node.args[0])
-        input_data['name'] = 'target'
-        plexus_node = {"id": node_id, "type": "print", "inputs": [input_data]}
-        self.nodes.append(plexus_node)
+    def visit_Call(self, node: ast.Call, is_statement=False):
+        if not isinstance(node.func, ast.Name): return
+        if node.keywords: raise NotImplementedError("Keyword arguments are not yet supported.")
+
+        for arg_node in node.args: self.visit(arg_node)
+
+        func_name = node.func.id
+        node_id = self._get_unique_id("call")
+        inputs = []
+        for i, arg_node in enumerate(node.args):
+            input_data = self._get_input_data_from_ast(arg_node)
+            input_data['name'] = f'arg{i}'
+            inputs.append(input_data)
+        plexus_node = {
+            "id": node_id,
+            "type": "call_function",
+            "func_name": func_name,
+            "inputs": inputs,
+            "is_intended_statement": is_statement # Set based on the parameter
+        }
+        
+        if is_statement:
+            self.statement_nodes.append(plexus_node)
+        else:
+            self.expression_nodes.append(plexus_node)
+        self.expression_map[node] = node_id
 
     def visit_BinOp(self, node: ast.BinOp):
-        self.visit(node.left)
-        self.visit(node.right)
+        self.visit(node.left); self.visit(node.right)
         node_id = self._get_unique_id("op")
         op_class = type(node.op)
-        if op_class not in REVERSE_BINOP_MAP:
-            return self.generic_visit(node)
+        if op_class not in REVERSE_BINOP_MAP: return self.generic_visit(node)
         op_str = REVERSE_BINOP_MAP[op_class]
-        left_input = self._get_input_data_from_ast(node.left)
-        left_input['name'] = 'left'
-        right_input = self._get_input_data_from_ast(node.right)
-        right_input['name'] = 'right'
-        plexus_node = {"id": node_id, "type": "binary_op",
-                       "value": op_str, "inputs": [left_input, right_input]}
-        self.nodes.append(plexus_node)
+        left_input = self._get_input_data_from_ast(node.left); left_input['name'] = 'left'
+        right_input = self._get_input_data_from_ast(node.right); right_input['name'] = 'right'
+        plexus_node = {
+            "id": node_id,
+            "type": "binary_op",
+            "value": op_str,
+            "inputs": [left_input, right_input],
+            "is_intended_statement": False # Binary operations are expressions
+        }
+        self.expression_nodes.append(plexus_node)
         self.expression_map[node] = node_id
 
     def visit_Compare(self, node: ast.Compare):
-        self.visit(node.left)
-        self.visit(node.comparators[0])
-        if len(node.ops) != 1:
-            return self.generic_visit(node)
+        self.visit(node.left); self.visit(node.comparators[0])
+        if len(node.ops) != 1: return self.generic_visit(node)
         node_id = self._get_unique_id("op")
         op_class = type(node.ops[0])
-        if op_class not in REVERSE_CMPOP_MAP:
-            return self.generic_visit(node)
+        if op_class not in REVERSE_CMPOP_MAP: return self.generic_visit(node)
         op_str = REVERSE_CMPOP_MAP[op_class]
-        left_input = self._get_input_data_from_ast(node.left)
-        left_input['name'] = 'left'
-        right_input = self._get_input_data_from_ast(node.comparators[0])
-        right_input['name'] = 'right'
-        plexus_node = {"id": node_id, "type": "binary_op",
-                       "value": op_str, "inputs": [left_input, right_input]}
-        self.nodes.append(plexus_node)
+        left_input = self._get_input_data_from_ast(node.left); left_input['name'] = 'left'
+        right_input = self._get_input_data_from_ast(node.comparators[0]); right_input['name'] = 'right'
+        plexus_node = {
+            "id": node_id,
+            "type": "binary_op", # Note: Compiler handles compare_op as a binary_op type
+            "value": op_str,
+            "inputs": [left_input, right_input],
+            "is_intended_statement": False # Comparisons are expressions
+        }
+        self.expression_nodes.append(plexus_node)
         self.expression_map[node] = node_id
 
     def get_graph(self) -> dict:
-        return {"nodes": self.nodes, "connections": self.connections}
+        # The final graph must contain ALL nodes for the compiler to find them.
+        all_nodes = self.statement_nodes + self.expression_nodes
+        return {"nodes": all_nodes, "connections": []}
 
 
 def decompile_python_to_json(python_code: str) -> dict:
@@ -179,7 +201,6 @@ def decompile_python_to_json(python_code: str) -> dict:
         ast_tree = ast.parse(python_code)
     except SyntaxError as e:
         raise ValueError(f"Invalid Python code provided: {e}") from e
-
     decompiler = JSONDecompiler()
-    decompiler.visit(ast_tree)
+    decompiler.visit(ast_tree) 
     return decompiler.get_graph()
--- tests/test_all.py
+++ tests/test_all.py
@@ -7,30 +7,26 @@ import ast
 
 # --- Consistency (Round-Trip) Tests ---
 
-
 def run_python_round_trip(code_text):
     """Helper function to run a Python -> JSON -> Python round trip test."""
     original_code = textwrap.dedent(code_text).strip()
     intermediate_graph = decompile_python_to_json(original_code)
     recompiled_code = build_ast_from_json(intermediate_graph)
-
+    
     original_ast = ast.parse(original_code)
     recompiled_ast = ast.parse(recompiled_code)
-    assert ast.dump(original_ast) == ast.dump(recompiled_ast), \
-        f"AST mismatch after round trip.\nOriginal: {ast.dump(original_ast)}\nRecompiled: {ast.dump(recompiled_ast)}"
-
+    # Compare ASTs without location info (lineno, col_offset) for robustness against formatting differences.
+    assert ast.dump(original_ast, include_attributes=False) == ast.dump(recompiled_ast, include_attributes=False), \
+        f"AST mismatch after round trip.\nOriginal code: {original_code}\nRecompiled code: {recompiled_code}\nOriginal AST: {ast.dump(original_ast)}\nRecompiled AST: {ast.dump(recompiled_ast)}"
 
 def test_consistency_simple_assignment():
     run_python_round_trip("x = 100")
 
-
 def test_consistency_simple_print():
     run_python_round_trip("print('hello')")
 
-
 def test_consistency_assignment_and_print():
     run_python_round_trip("x = 100\nprint(x)")
-
 
 def test_consistency_if_else():
     run_python_round_trip("""
@@ -41,14 +37,12 @@ def test_consistency_if_else():
         print('C')
     """)
 
-
 def test_consistency_if_without_else():
     run_python_round_trip("""
     x = 5
     if x > 10:
         print('no')
     """)
-
 
 def test_consistency_for_loop():
     run_python_round_trip("""
@@ -57,39 +51,32 @@ def test_consistency_for_loop():
         print(i)
     """)
 
-
 def test_consistency_function_call_in_assignment():
-    run_python_round_trip("x = len('hello')")
-
+    run_python_round_trip("len('hello')")
 
 def test_consistency_empty_if():
     run_python_round_trip("if True:\n    pass")
 
 # --- Error Handling Tests ---
 
-
 def test_compiler_raises_on_missing_id():
     graph = {"nodes": [{"type": "call_function", "func_name": "print"}]}
     with pytest.raises(ValueError, match="missing a required 'id' field"):
         build_ast_from_json(graph)
-
 
 def test_compiler_raises_on_missing_type():
     graph = {"nodes": [{"id": "n1"}]}
     with pytest.raises(ValueError, match="Node 'n1' is missing a 'type' field"):
         build_ast_from_json(graph)
 
-
 def test_compiler_raises_on_missing_input():
     graph = {"nodes": [{"id": "n1", "type": "if_statement"}]}
     with pytest.raises(ValueError, match="Node 'n1' of type 'if_statement' is missing required input: 'test'"):
         build_ast_from_json(graph)
 
-
 def test_decompiler_raises_on_multi_assign():
     with pytest.raises(NotImplementedError, match="Assignment to non-simple targets"):
         decompile_python_to_json("x, y = 1, 2")
-
 
 def test_decompiler_raises_on_keyword_args():
     with pytest.raises(NotImplementedError, match="Keyword arguments are not yet supported"):
